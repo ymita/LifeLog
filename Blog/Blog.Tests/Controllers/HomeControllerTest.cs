c228adfea5d33084c5bbe2871ea5794d255e807a
Yuki Mita <Administrator@WIN-K6LU61KOAUP>
2017-03-19 11:31:31 +0900

    Added resource files
    Added resources files (default and ja) for English and Japanese

--- Blog/Blog.Tests/Controllers/HomeControllerTest.cs
+++ Blog/Blog.Tests/Controllers/HomeControllerTest.cs
@@ -19,7 +19,7 @@ namespace Blog.Tests.Controllers
             HomeController controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Index() as ViewResult;
+            ViewResult result = controller.Index(1) as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);

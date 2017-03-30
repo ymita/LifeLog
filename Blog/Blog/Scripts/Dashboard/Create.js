8b59e7cef379ec5ec635a1e966139fad9e38fb13
ymita <Administrator@WIN-K6LU61KOAUP>
2017-03-30 22:06:41 +0900

    Added a feature to fill datetime on "Published" field

---
+++ Blog/Blog/Scripts/Dashboard/Create.js
@@ -0,0 +1,17 @@
+﻿window.onload = function () {
+
+    var date = new Date();
+    formattedDate = [
+        date.getFullYear(),
+        ('0' + (date.getMonth() + 1)).slice(-2),
+        ('0' + date.getDate()).slice(-2)
+    ].join('-');
+
+    formattedTime = [
+        ('0' + date.getHours()).slice(-2),
+        ('0' + date.getMinutes()).slice(-2)
+    ].join(':');
+
+    var published = document.getElementById("Published");
+    published.value = formattedDate + " " + formattedTime;
+}

dba6d5e284ce181fa239d17d45a7443d9e2bef15
ymita <Administrator@WIN-K6LU61KOAUP>
2017-03-30 21:30:44 +0900

    Moved script code to an external js file

---
+++ Blog/Blog/Scripts/dashboard.js
@@ -0,0 +1,9 @@
+﻿window.onload = function () {
+    var dialogButton = document.getElementById("dialogButton");
+    dialogButton.onclick = function () {
+        var hiddenIdElem = document.getElementById("id");
+        var _idText = this.parentElement.parentElement.cells["0"].textContent;
+        var _id = parseInt(_idText);
+        hiddenIdElem.value = _id;
+    }
+}
